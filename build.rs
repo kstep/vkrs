0989443b19dd79d4be8f81098f24c39d892d6445
Konstantin Stepanov <me@kstep.me>
2016-01-27 16:33:38 +0300

    add build script

---
+++ build.rs
@@ -0,0 +1,32 @@
+#[cfg(feature = "nightly")]
+mod inner {
+    pub fn main() {}
+}
+
+#[cfg(not(feature = "nightly"))]
+mod inner {
+    extern crate syntex;
+    extern crate serde_codegen;
+
+    use std::env;
+    use std::path::Path;
+    use std::fs;
+
+    pub fn main() {
+        let outdir = env::var_os("OUT_DIR").unwrap();
+        for src in fs::read_dir("src").unwrap()
+            .filter_map(Result::ok)
+            .filter(|s| s.file_type().unwrap().is_file())
+            .map(|s| s.path())
+            .filter(|s| s.extension().unwrap() == "in") {
+                let dst = Path::new(&outdir).join(src.file_stem().unwrap());
+                let mut registry = syntex::Registry::new();
+                serde_codegen::register(&mut registry);
+                registry.expand("", &src, &dst).unwrap();
+        }
+    }
+}
+
+fn main() {
+    inner::main();
+}
