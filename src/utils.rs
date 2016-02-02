20e555fb55f7812e7bbd0ce0ce30b4bb40e8f8e5
Konstantin Stepanov <me@kstep.me>
2016-02-03 02:36:22 +0300

    more derives

--- src/utils.rs
+++ src/utils.rs
@@ -5,17 +5,20 @@ include!("utils.rs.in");
 include!(concat!(env!("OUT_DIR"), "/utils.rs"));
 
 request_ref! {
+    #[derive(Copy, Eq)]
     struct CheckLink for ["utils.checkLink"](v => 5.44) -> LinkStatus {
         url: str = ("") => {=},
     }
 }
 
 request_ref! {
+    #[derive(Copy, Eq)]
     struct ResolveScreenName for ["utils.resolveScreenName"](v => 5.44) -> Option<ScreenNameInfo> {
         screen_name: str = ("") => {=},
     }
 }
 
 request! {
+    #[derive(Copy, Eq)]
     struct GetServerTime for ["utils.getServerTime"](v => 5.44) -> u64;
 }
