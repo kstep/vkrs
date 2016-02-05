c8bd708c69d0aea7c955ede9630a7bcf728b4363
Konstantin Stepanov <me@kstep.me>
2016-02-05 22:16:46 +0300

    account.getCounters method

--- src/account.rs
+++ src/account.rs
@@ -1,9 +1,34 @@
 use auth::Permissions;
 use api::Id;
 
+#[cfg(feature = "unstable")]
+include!("account.rs.in");
+
+#[cfg(not(feature = "unstable"))]
+include!(concat!(env!("OUT_DIR"), "/account.rs"));
+
 request! {
     #[derive(Eq, Copy)]
     struct GetAppPermissions for ["account.getAppPermissions"](v => 5.44) -> Permissions {
         user_id: Id = () => {}
     }
 }
+
+request_ref! {
+    #[derive(Eq, Copy)]
+    struct GetCounters for ["account.getCounters"](v => 5.44) -> Counters {
+        filter: [Filter] = (&[][..]) => {AsRef<Vec>},
+    }
+}
+
+enum_str! { Filter {
+    Friends = "friends",
+    Messages = "messages",
+    Photos = "photos",
+    Videos = "videos",
+    Notes = "notes",
+    Gifts = "gifts",
+    Events = "events",
+    Groups = "groups",
+    Sdk = "sdk",
+}}
