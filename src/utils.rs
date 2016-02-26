f04e96d95a290ca534ab5a3c2dbac3a45a2f7b14
Konstantin Stepanov <me@kstep.me>
2016-02-26 15:09:45 +0300

    utils.checkLink uses enum for link status

--- src/utils.rs
+++ src/utils.rs
@@ -8,7 +8,7 @@ include!(concat!(env!("OUT_DIR"), "/utils.rs"));
 
 request_ref! {
     #[derive(Copy, Eq)]
-    struct CheckLink for ["utils.checkLink"](v => 5.44) -> LinkStatus {
+    struct CheckLink for ["utils.checkLink"](v => 5.44) -> LinkInfo {
         url: str = ("") => {=},
     }
 }
